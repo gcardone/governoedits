9fec925818c4ce90cb4576f2137c9ee7168dc65c
Andrea Reale <andrea.reale@gmail.com>
2014-07-31 16:45:12 +0200

    Added properties interface

---
+++ src/main/java/it/governoedits/util/PropertyNotFoundException.java
@@ -0,0 +1,11 @@
+package it.governoedits.util;
+
+public final class PropertyNotFoundException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+
+    public PropertyNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
